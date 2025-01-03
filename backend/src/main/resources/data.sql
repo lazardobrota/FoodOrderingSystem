d0aa6e7167001c780f5be992d20c10205ccb888c
lazardobrota <lazardobrota1323@gmail.com>
2025-01-03 16:30:25 +0100

    backend: setup database

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into user_management.user (name, lastname, email, password, permissions_bit_mask) values ('admin', 'admin', 'admin@gmail.com', 'admin', 15);
-insert into user_management.user (name, lastname, email, password, permissions_bit_mask) values ('Lazar', 'Dobrota', 'ldobrota@raf.rs', '1234', 1);
-insert into user_management.user (name, lastname, email, password, permissions_bit_mask) values ('Lazar', 'Dobrota', '2ldobrota@raf.rs', '1234', 0);
-insert into user_management.user (name, lastname, email, password, permissions_bit_mask) values ('John', 'Black', 'jb@gmail.com', '1234', 7);
-insert into user_management.user (name, lastname, email, password, permissions_bit_mask) values ('Ime', 'Prezime', 'ip@gmail.com', '1234', 15);
+insert into food_ordering_system.user (name, lastname, email, password, permissions_bit_mask) values ('admin', 'admin', 'admin@gmail.com', 'admin', 15);
+insert into food_ordering_system.user (name, lastname, email, password, permissions_bit_mask) values ('Lazar', 'Dobrota', 'ldobrota@raf.rs', '1234', 1);
+insert into food_ordering_system.user (name, lastname, email, password, permissions_bit_mask) values ('Lazar', 'Dobrota', '2ldobrota@raf.rs', '1234', 0);
+insert into food_ordering_system.user (name, lastname, email, password, permissions_bit_mask) values ('John', 'Black', 'jb@gmail.com', '1234', 7);
+insert into food_ordering_system.user (name, lastname, email, password, permissions_bit_mask) values ('Ime', 'Prezime', 'ip@gmail.com', '1234', 15);
--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-create table user_management.user
+create table food_ordering_system.user
 (
     id                   bigint       not null auto_increment,
     name                 varchar(255) not null,
@@ -9,4 +9,42 @@ create table user_management.user
     primary key (id)
 );
 
-delete from user_management.user;
+create table dish
+(
+    id   bigint       not null auto_increment,
+    name varchar(255) not null unique,
+    primary key (id)
+);
+
+create table food_ordering_system.`order`
+(
+    id        bigint  not null auto_increment primary key,
+    status    int     not null,
+    createdBy bigint  not null,
+    active    bool not null,
+    constraint user___fk
+        foreign key (createdBy) references user (id) on delete cascade
+);
+
+create table food_ordering_system.order_dish
+(
+    id       bigint not null auto_increment primary key,
+    order_id bigint not null,
+    dish_id  bigint not null,
+    constraint order__fk
+        foreign key (order_id) references `order` (id) on delete cascade,
+    constraint dish__fk
+        foreign key (dish_id) references dish (id) on delete cascade
+);
+
+#Empty Table
+delete from food_ordering_system.user;
+delete from food_ordering_system.dish;
+delete from food_ordering_system.order;
+delete from food_ordering_system.order_dish;
+
+#Delete Table
+drop table food_ordering_system.user;
+drop table food_ordering_system.dish;
+drop table food_ordering_system.order;
+drop table food_ordering_system.order_dish;
